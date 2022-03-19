9bb35c56169a511668075c83dfb1e568e1610b63
br-mat <76951337+br-mat@users.noreply.github.com>
2022-03-19 23:24:15 +0100

    Update lec4.py

--- code/lec4.py
+++ code/lec4.py
@@ -1,13 +1,19 @@
 from machine import Pin, I2C, ADC
 import time
 
-#defined global variables (replace values)
-_up = 3000
-_low = 1000
-threshold = 1500 #example value
+#defined global variables
+_up = 44068.67
+_low = 17736.8
+limit = 30000.0
 
-
+#set analog pin
 analog_value = ADC(28)
+
+# function calculate relative moisture in percent
+def moisture(val):
+    perc = int(100-(val-_low)*100/(_up-_low)) #between 0 and 100
+    print(f"Soil Moisture: {perc}% ")
+    return perc
 
 #take multiple measurements
 vals=[0] * 60 #create list with 60 positions
@@ -16,11 +22,13 @@ for index, element in enumerate(vals): #iterate over list
 val = sum(vals) / len(vals) #derive the average
 
 #decide if measurement is ok
+print (moisture (val))
+#print (val)
 if _low <= val <= _up:
     if val > limit:
         #do something
-        print('do something')
+        print('I need water!')
     else:
-        print('I feel fine')
+        print('I feel fine ... maybee to ')
 else:
-    print('Warning: Measurement went wrong')
+    print(f'Warning: Measurement went wrong {val}')
